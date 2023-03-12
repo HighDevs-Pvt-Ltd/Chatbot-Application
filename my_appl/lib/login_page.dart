98fc0dbc57b7bfba0918563ffd70197ce20f0fac
Ab Dul <abdulbasit22297@gmail.com>
2023-03-12 22:12:19 +0500

    Modified login page

--- my_appl/lib/login_page.dart
+++ my_appl/lib/login_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: const Text('Login Page'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Login Page'),
       ),
     );
